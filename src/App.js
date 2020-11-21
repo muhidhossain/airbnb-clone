32d62bd85b1b620c9324643ad94f86230099ddec
Muhid Hossain <muhidhossain7@gmail.com>
2020-11-22 00:48:03 +0600

    airbnb clone done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SearchPage from './Components/SearchPage/SearchPage';
+import ScrollToTop from './Components/ScrollToTop/ScrollToTop';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    // BEM
+    <div className="app">
+      <Router>
+        <ScrollToTop />
+        <Header />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/search'>
+            <SearchPage />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
